220b31929bd6e6241ba1d78e70ef93912faeaa0f
Ulises <larrabeulises@gmail.com>
2025-01-27 14:54:08 -0300

    Add Vercel adapter to Astro configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import vercelAdapter from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  output: 'server',
+  adapter: vercelAdapter(),
 });
